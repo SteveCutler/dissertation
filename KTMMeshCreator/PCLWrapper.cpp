b63686bb4df4d02e8a0c7eabcaffc3ad4db5fd34
ganterd <ganterd@tcd.ie>
2013-05-03 16:33:02 +0100

    Recorded Test Files & Started on Cloud Registration

--- KTMMeshCreator/PCLWrapper.cpp
+++ KTMMeshCreator/PCLWrapper.cpp
@@ -1,8 +1,10 @@
 #include "PCLWrapper.hpp"
 
 KTM::PCLWrapper::PCLWrapper(){
+	mergedCloud = pcl::PointCloud<pcl::PointXYZ>::Ptr(new pcl::PointCloud<pcl::PointXYZ>);
 	cloudViewer = new pcl::visualization::CloudViewer("Cloud Viewer");
-	p_mergedCloud = new boost::shared_ptr<pcl::PointCloud<pcl::PointXYZ>>(&mergedCloud);
+	firstRun = true;
+	GlobalTransform = Eigen::Matrix4f::Identity();
 }
 
 KTM::PCLWrapper::~PCLWrapper(){
@@ -16,19 +18,145 @@ bool KTM::PCLWrapper::addToCloud(unsigned short* depthData, int depthDataWidth, 
 	if(NULL == depthData)
 		return false;
 	
-	mergedCloud.clear();
+	NUI_IMAGE_RESOLUTION depthRes;
+	switch(depthDataWidth){
+	case 640:
+		depthRes = NUI_IMAGE_RESOLUTION_640x480;
+		break;
+	case 320:
+		depthRes = NUI_IMAGE_RESOLUTION_320x240;
+		break;
+	case 80:
+		depthRes = NUI_IMAGE_RESOLUTION_80x60;
+		break;
+	}
+
+	pcl::PointCloud<pcl::PointXYZ>::Ptr depthCloud (new pcl::PointCloud<pcl::PointXYZ>);
+	depthCloud->resize(depthDataWidth * depthDataHeight);
 
 	if(NULL != depthData){
 		int index = 0;
 		for(int dCol = 0; dCol < depthDataHeight; dCol++){
 			for(int dRow = 0; dRow < depthDataWidth; dRow++){
-				unsigned short d = depthData[index++];
-				if(d > 0)
-					mergedCloud.push_back(pcl::PointXYZ(dRow, dCol, d));
+				unsigned short d = depthData[index];
+				if(d > 0){
+					NUI_DEPTH_IMAGE_POINT depthPoint;
+					depthPoint.depth = d;
+					depthPoint.x = dCol;
+					depthPoint.y = dRow;
+					Vector4 p = NuiTransformDepthImageToSkeleton(dRow, dCol, d, depthRes);
+					depthCloud->points[index].x = p.x;
+					depthCloud->points[index].y = p.y;
+					depthCloud->points[index].z = p.z;
+				}
+				index++;
 			}
 		}
 	}
 
-	cloudViewer->showCloud(*p_mergedCloud, "Cloud");
+	if(firstRun){
+		mergedCloud = depthCloud;
+		firstRun = false;
+	}else{
+		Eigen::Matrix4f pairTransform;
+		pcl::PointCloud<pcl::PointXYZ>::Ptr tmp(new pcl::PointCloud<pcl::PointXYZ>);
+		pairAlign(depthCloud, mergedCloud, tmp, pairTransform, true);
+		pcl::transformPointCloud(*tmp, *mergedCloud, GlobalTransform);
+		GlobalTransform = pairTransform * GlobalTransform;
+	}
+
+	cloudViewer->showCloud(mergedCloud, "Cloud");
 	return true;
+}
+
+void KTM::PCLWrapper::pairAlign (const PointCloud::Ptr cloud_src, const PointCloud::Ptr cloud_tgt, PointCloud::Ptr output, Eigen::Matrix4f &final_transform, bool downsample)
+{
+  //
+  // Downsample for consistency and speed
+  // \note enable this for large datasets
+  PointCloud::Ptr src (new PointCloud);
+  PointCloud::Ptr tgt (new PointCloud);
+  pcl::VoxelGrid<PointT> grid;
+  if (downsample)
+  {
+    grid.setLeafSize (0.05, 0.05, 0.05);
+    grid.setInputCloud (cloud_src);
+    grid.filter (*src);
+
+    grid.setInputCloud (cloud_tgt);
+    grid.filter (*tgt);
+  }
+  else
+  {
+    src = cloud_src;
+    tgt = cloud_tgt;
+  }
+
+
+  // Compute surface normals and curvature
+  PointCloudWithNormals::Ptr points_with_normals_src (new PointCloudWithNormals);
+  PointCloudWithNormals::Ptr points_with_normals_tgt (new PointCloudWithNormals);
+
+  pcl::NormalEstimation<PointT, PointNormalT> norm_est;
+  pcl::search::KdTree<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ> ());
+  norm_est.setSearchMethod (tree);
+  norm_est.setKSearch (30);
+  
+  norm_est.setInputCloud (src);
+  norm_est.compute (*points_with_normals_src);
+  pcl::copyPointCloud (*src, *points_with_normals_src);
+
+  norm_est.setInputCloud (tgt);
+  norm_est.compute (*points_with_normals_tgt);
+  pcl::copyPointCloud (*tgt, *points_with_normals_tgt);
+
+  //
+  // Instantiate our custom point representation (defined above) ...
+  KTM::PointAndCurvatureRepresentation point_representation;
+  // ... and weight the 'curvature' dimension so that it is balanced against x, y, and z
+  float alpha[4] = {1.0, 1.0, 1.0, 1.0};
+  point_representation.setRescaleValues (alpha);
+
+  //
+  // Align
+  pcl::IterativeClosestPointNonLinear<PointNormalT, PointNormalT> reg;
+  reg.setTransformationEpsilon (1e-6);
+  // Set the maximum distance between two correspondences (src<->tgt) to 10cm
+  // Note: adjust this based on the size of your datasets
+  reg.setMaxCorrespondenceDistance (0.1);  
+  // Set the point representation
+  reg.setPointRepresentation (boost::make_shared<const KTM::PointAndCurvatureRepresentation> (point_representation));
+
+  reg.setInputCloud (points_with_normals_src);
+  reg.setInputTarget (points_with_normals_tgt);
+
+
+
+  //
+  // Run the same optimization in a loop and visualize the results
+  Eigen::Matrix4f Ti = Eigen::Matrix4f::Identity (), prev, targetToSource;
+  PointCloudWithNormals::Ptr reg_result = points_with_normals_src;
+  reg.setMaximumIterations (2);
+  for (int i = 0; i < 30; ++i)
+  {
+    PCL_INFO ("Iteration Nr. %d.\n", i);
+
+    // save cloud for visualization purpose
+    points_with_normals_src = reg_result;
+
+    // Estimate
+    reg.setInputCloud (points_with_normals_src);
+    reg.align (*reg_result);
+
+		//accumulate transformation between each Iteration
+    Ti = reg.getFinalTransformation () * Ti;
+
+		//if the difference between this transformation and the previous one
+		//is smaller than the threshold, refine the process by reducing
+		//the maximal correspondence distance
+    if (fabs ((reg.getLastIncrementalTransformation () - prev).sum ()) < reg.getTransformationEpsilon ())
+      reg.setMaxCorrespondenceDistance (reg.getMaxCorrespondenceDistance () - 0.001);
+    
+    prev = reg.getLastIncrementalTransformation ();
+  }
 }
--- KTMMeshCreator/PCLWrapper.hpp
+++ KTMMeshCreator/PCLWrapper.hpp
@@ -1,16 +1,62 @@
 #pragma once
 
-#pragma comment( lib, "pcl_visualization_debug.lib" )
-
+#define NOMINMAX
+#include <Windows.h>
 #include <pcl/visualization/cloud_viewer.h>
+#include <boost/make_shared.hpp>
 #include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_representation.h>
+
+#include <pcl/io/pcd_io.h>
+
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/filter.h>
+
+#include <pcl/features/normal_3d.h>
+
+#include <pcl/registration/icp.h>
+#include <pcl/registration/icp_nl.h>
+#include <pcl/registration/transforms.h>
+
+#include <pcl/visualization/pcl_visualizer.h>
+#include <KinectWrapper.hpp>
+#include <NuiApi.h>
+
+typedef pcl::PointXYZ PointT;
+typedef pcl::PointCloud<PointT> PointCloud;
+typedef pcl::PointNormal PointNormalT;
+typedef pcl::PointCloud<PointNormalT> PointCloudWithNormals;
 
 namespace KTM{
+	class PointAndCurvatureRepresentation : public pcl::PointRepresentation <PointNormalT>{
+	  using pcl::PointRepresentation<PointNormalT>::nr_dimensions_;
+	public:
+	  PointAndCurvatureRepresentation ()
+	  {
+		// Define the number of dimensions
+		nr_dimensions_ = 4;
+	  }
+
+	  // Override the copyToFloatArray method to define our feature vector
+	  virtual void copyToFloatArray (const PointNormalT &p, float * out) const
+	  {
+		// < x, y, z, curvature >
+		out[0] = p.x;
+		out[1] = p.y;
+		out[2] = p.z;
+		out[3] = p.curvature;
+	  }
+	};
+
 	class PCLWrapper{
 	private:
-		pcl::PointCloud<pcl::PointXYZ> mergedCloud;
-		boost::shared_ptr<pcl::PointCloud<pcl::PointXYZ>>* p_mergedCloud;
+		pcl::PointCloud<pcl::PointXYZ>::Ptr mergedCloud;
 		pcl::visualization::CloudViewer* cloudViewer;
+		Eigen::Matrix4f GlobalTransform;
+		bool firstRun;
+
+		void pairAlign (const PointCloud::Ptr cloud_src, const PointCloud::Ptr cloud_tgt, PointCloud::Ptr output, Eigen::Matrix4f &final_transform, bool downsample);
 	public:
 		PCLWrapper();
 		~PCLWrapper();
